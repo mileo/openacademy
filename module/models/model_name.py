266bda6a53d959d832814ab67bc349126475de48
Daniel Torres <danieltorres7@gmail.com>
2016-03-30 16:39:00 -0300

    Deleted module named Module

--- module/models/model_name.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# © <YEAR(S)> <AUTHOR(S)>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from openerp import models
-
-
-class ModelName(models.Model):
-    _inherit = "model.name"
