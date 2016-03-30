a2e13a5924e5b75ec11da5c53bc67e963b43f491
Luis Felipe Mileo <mileo@kmee.com.br>
2016-03-30 17:25:00 -0300

    Exercicio 8

--- openacademy/models/__init__.py
+++ openacademy/models/__init__.py
@@ -1,1 +1,3 @@
 from . import course
+from . import attendee
+from . import session
---
+++ openacademy/models/attendee.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# © <2016> <Luis Felipe Mileo>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from openerp import fields, models
+
+
+class Attendee(models.Model):
+
+    _name = "openacademy.attendee"
+    _description = "Attendee"
+
+    name = fields.Char(
+        string="Name",
+        help="Insira o nome do Attendee"
+    )
