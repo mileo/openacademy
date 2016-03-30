97ce18e8695c64b29fd103b61a6599edd8f81197
Luis Felipe Mileo <mileo@kmee.com.br>
2016-03-30 17:51:01 -0300

    Fix pep8

--- openacademy/models/course.py
+++ openacademy/models/course.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
+# © <2016> <Luis Felipe Mileo>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 from openerp import models, fields
 
 
 class Course(models.Model):
-    """
-       
-    """
     _name = 'openacademy.course'
     _description = "Course"    
 
