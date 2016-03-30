307269bd73238414ed67221a69da4eeb141f7e23
Daniel Torres <danieltorres7@gmail.com>
2016-03-30 12:44:04 -0300

    Updates

--- openacademy/__openerp__.py
+++ openacademy/__openerp__.py
@@ -1,36 +1,21 @@
 # -*- coding: utf-8 -*-
+# © 2016 - KMEE INFORMATICA LTDA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    'name': "Open Academy",
-
-    'summary': """Manage trainings""",
-
-    'description': """
-        Open Academy module for managing trainings:
-            - training courses
-            - training sessions
-            - attendees registration
-    """,
-
-    'author': "Your Company",
-    'website': "http://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
-    # for the full list
-    'category': 'Test',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base'],
-
-    # always loaded
-    'data': [
-        # 'security/ir.model.access.csv',
-        'templates.xml',
-        'views/openacademy.xml',
+    "name": "OpenAcademy",
+    "summary": "My OpenAcademy",
+    "version": "8.0.1.0.0",
+    "category": "Custom",
+    "website": "https://www.kmee.com.br",
+    "author": "Daniel Torres, KMEE, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "base",
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo.xml',
+    "data": [
+        # 'security/ir.model.access.csv',
+        "views/openacademy.xml",
     ],
 }
