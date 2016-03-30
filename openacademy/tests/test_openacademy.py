3ab6a286e3c87dfb1248aba2476154778fdc6f8a
Luis Felipe Mileo <mileo@kmee.com.br>
2016-03-30 16:21:41 -0300

    [NEW] Exercicio 2

--- openacademy/tests/test_openacademy.py
+++ openacademy/tests/test_openacademy.py
@@ -2,42 +2,38 @@
 # © 2015 Luis Felipe Mileo
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-import openerp.tests.common as common
+from openerp.tests.common import TransactionCase
 from openerp import fields
 
 
-class TestOpenAcademy(common.TransactionCase):
+class Tests(TransactionCase):
     def setUp(self):
-        super(TestOpenAcademy, self).setUp()
-        self.res_partner_model = self.env['res.partner']
-        responsible = self.res_partner_model.create({
-            'name': 'Test Responsible',
-            'customer': True,
-            'supplier': True,
-            'lang': 'en_US',
+
+        super(Tests, self).setUp()
+        self.responsible_1_id = self.ref(
+            'base.res_partner_address_35')
+        self.instructor_1_id = self.ref(
+            'base.res_partner_address_34')
+
+    def test_01_create_attendee(self):
+        self.attendee_model = self.env['openacademy.attendee']
+        self.attendee_01_id = self.attendee_model.create({
+            'name': 'Attendee 01',
+            'partner_id': self.responsible_1_id,
         })
-        instructor = self.res_partner_model.create({
-            'name': 'Test Instructor',
-            'start_date': True,
-            'supplier': True,
-            'lang': 'en_US',
-        })
-        self.course_model = self.env['openacademy.course']
-        course = self.course_model.create({
-            'name': 'CCO',
-            'description': 'Ciencias da computacao',
-            'responsible_id': responsible.id,
-        })
-        self.session_model = self.env['openacademy.session']
-        self.session_model.create({
-            'name': '2016/1',
-            'description': 'Ciencias da computacao',
-            'start_date': fields.Date.today(),
-            'duration': 60,
-            'instructor_id': instructor.id,
-            'course_id': course.id,
-            'attendee_ids': [(0, 0, {
-                'partner_id': self.ref(
-                    'sale_commission.res_partner_pritesh_sale_agent'),
-            })]
-        })
+    #
+    # def test_02_create_session(self):
+    #
+    #     self.session_model = self.env['openacademy.session']
+    #     self.session_model.create({
+    #         'name': '2016/1',
+    #         'description': 'Ciencias da computacao',
+    #         'start_date': fields.Date.today(),
+    #         'duration': 60,
+    #         'instructor_id': self.instructor_1_id,
+    #         'course_id': self.course_01_id.id,
+    #         'attendee_ids': [(0, 0, {
+    #             'partner_id': self.ref(
+    #                 'sale_commission.res_partner_pritesh_sale_agent'),
+    #         })]
+    #     })
